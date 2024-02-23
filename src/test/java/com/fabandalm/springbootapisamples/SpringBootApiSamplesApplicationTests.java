99cdef3437afb64e031488aec39f38218dc81abf
Fabio Almeida <fabandalm@gmail.com>
2024-02-23 08:49:07 -0500

    Added initial files

---
+++ src/test/java/com/fabandalm/springbootapisamples/SpringBootApiSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabandalm.springbootapisamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
